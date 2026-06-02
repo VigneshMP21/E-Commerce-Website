a439f6858dd404d00aa97b9395a8ced074b3fca0
VigneshMP21 <mpvignesh06@gmail.com>
2026-06-03 01:46:32 +0530

    Updated file

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import toast from 'react-hot-toast';
 import { clearAuthTokens, getStoredToken } from '../utils/authStorage';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   headers: { 'Content-Type': 'application/json' },
   timeout: 30000
 });
